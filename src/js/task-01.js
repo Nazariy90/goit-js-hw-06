5a54bf457199c9585a1637191f91f08f32bf6716
Nazariy90 <pinhvin123@gmail.com>
2023-03-20 21:17:31 +0200

    add task 01

---
+++ src/js/task-01.js
@@ -0,0 +1,15 @@
+// "use strict";
+
+const sumCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${sumCategories.length}`);
+
+const items = document.querySelectorAll(".item");
+
+Array.prototype.forEach.call(items, (element) => {
+  const title = element.querySelector("h2").innerHTML;
+
+  const itemsSum = element.querySelectorAll("li").length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${itemsSum}`);
+});
