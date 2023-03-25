1ef597307685ec4bb46671a812908c5739951f62
Nazariy90 <pinhvin123@gmail.com>
2023-03-25 12:30:13 +0200

    add change

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,13 +1,11 @@
 const inputEl = document.querySelector("#validation-input");
-const lengthEl = inputEl.dataset.length;
+const lengthEl = Number(inputEl.dataset.length);
 
 const counterFunc = () => {
-  if (inputEl.value.length == lengthEl) {
-    inputEl.classList.add("valid");
-    inputEl.classList.remove("invalid");
+  if (inputEl.value.length === lengthEl) {
+    inputEl.classList.add("valid").remove("invalid");
   } else {
-    inputEl.classList.add("invalid");
-    inputEl.classList.remove("valid");
+    inputEl.classList.add("invalid").remove("valid");
   }
 };
 
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -8,8 +8,9 @@ function onSendForm(event) {
     elements: { email, password },
   } = event.currentTarget;
 
-  if (email.value === "" || password.value === "") {
-    return alert("всі поля повинні бути заповнені");
+  if (email.value === "" || !password.value) {
+    alert("всі поля повинні бути заповнені");
+    return;
   }
 
   objOLL.email = email.value;
